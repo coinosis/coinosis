25d3d2a8221d347d829837f52c57c8d1e785d0be
Emilio Silva Schlenker <emilio@autark.xyz>
2020-04-13 19:25:06 -0500

    Receive function & tests

--- contracts/Coinosis.sol
+++ contracts/Coinosis.sol
@@ -1,15 +1,27 @@
 pragma solidity 0.5.16;
 
 contract Coinosis {
-    
+
+    address payable constant coinosis =
+        0xe1fF19182deb2058016Ae0627c1E4660A895196a;
+    string constant ZERO_VALUE = "The ether value sent is zero (zero-value)";
+    string constant EMPTY_NAME = "The name provided is empty (empty-name)";
     string constant DIFFERENT_LENGTHS =
         "The number of recipients differs from the number of amounts \
 (different-lengths)";
     string constant INSUFFICIENT_VALUE =
         "The ether value sent is less than the total intended amount to send \
 (insufficient-value).";
-    
+
+    event Received(address sender, string name, uint amount);
     event Paid(address recipient, uint amount);
+
+    function receive(string memory name) public payable {
+        require(msg.value > 0, ZERO_VALUE);
+        require(bytes(name).length > 0, EMPTY_NAME);
+        coinosis.transfer(msg.value);
+        emit Received(msg.sender, name, msg.value);
+    }
     
     function distribute(
         address payable[] memory recipients,
@@ -22,7 +34,7 @@ contract Coinosis {
         }
         require(msg.value >= totalAmount, INSUFFICIENT_VALUE);
         for (uint i = 0; i < recipients.length; i++) {
-            recipients[i].transfer(amounts[i]); // TODO: change this to .call.value()()
+            recipients[i].transfer(amounts[i]);
             emit Paid(recipients[i], amounts[i]);
         }
     }
--- test/coinosis.js
+++ test/coinosis.js
@@ -1,72 +1,135 @@
 const Coinosis = artifacts.require('Coinosis');
+const truffleAssert = require('truffle-assertions');
 
 contract('Coinosis', async accounts => {
-  const nonDeployerAccounts = accounts.slice(1);
-  const getBalances = async accounts => {
-    const balances = [];
-    for (let i = 0; i < accounts.length; i++) {
-      const account = accounts[i];
-      const balance = await web3.eth.getBalance(account);
-      balances.push(BigInt(balance));
-    }
-    return balances;
-  }
-  it('should distribute one wei to every non-deployer account', async () => {
-    const initialBalances = await getBalances(nonDeployerAccounts);
-    const amounts = Array(nonDeployerAccounts.length).fill('1');
-    const instance = await Coinosis.deployed();
-    await instance.distribute(nonDeployerAccounts, amounts, {
-      value: String(nonDeployerAccounts.length),
+
+  contract('receive()', async accounts => {
+    const coinosisAccount = '0xe1fF19182deb2058016Ae0627c1E4660A895196a';
+    it('should transfer one wei to the coinosis account', async () => {
+      const initialBalance = await web3.eth.getBalance(coinosisAccount);
+      const instance = await Coinosis.deployed();
+      await instance.receive('foo', {value: 1});
+      const finalBalance = await web3.eth.getBalance(coinosisAccount);
+      assert.equal(finalBalance, (BigInt(initialBalance) + 1n).toString());
     });
-    const finalBalances = await getBalances(nonDeployerAccounts);
-    for (let i = 0; i < nonDeployerAccounts.length; i++) {
-      const initialBalance = initialBalances[i];
-      const finalBalance = finalBalances[i];
-      assert.equal(
-        finalBalance,
-        initialBalance + 1n
-      );
-    }
+    it('should emit a Received event with the right arguments', async () => {
+      const name = 'Hannah';
+      const sender = accounts[0];
+      const amount = 1;
+      const instance = await Coinosis.deployed();
+      const result = await instance.receive(name, {value: amount});
+      truffleAssert.eventEmitted(result, 'Received', event => {
+        return event.name === name
+          && event.sender === sender
+          && event.amount.toNumber() === amount;
+      })
+    });
+    it('should revert with a zero-value error', async () => {
+      let errorThrown = false;
+      const instance = await Coinosis.deployed();
+      try {
+        await instance.receive('foo');
+      }
+      catch (err) {
+        assert.include(
+          err.message,
+          'zero-value',
+          'The error thrown isn\'t zero-value: ' + err.message
+        );
+        errorThrown = true;
+      }
+      if (!errorThrown) {
+        assert.fail('No error thrown.');
+      }
+    });
+    it('should revert with a empty-name error', async () => {
+      let errorThrown = false;
+      const instance = await Coinosis.deployed();
+      try {
+        await instance.receive('', {value: 1});
+      }
+      catch (err) {
+        assert.include(
+          err.message,
+          'empty-name',
+          'The error thrown isn\'t empty-name: ' + err.message
+        );
+        errorThrown = true;
+      }
+      if (!errorThrown) {
+        assert.fail('No error thrown.');
+      }
+    });
   });
-  it('should revert with a different-lengths error',
-     async () => {
-       let errorThrown = false;
-       const instance = await Coinosis.deployed();
-       const amounts = Array(nonDeployerAccounts.length - 1).fill('1');
-       const value = nonDeployerAccounts.length * 2;
-       try {
-         await instance.distribute(nonDeployerAccounts, amounts, { value });
-       }
-       catch (err) {
-         assert.include(
-           err.message,
-           'different-lengths',
-           'The error thrown isn\'t different-lengths: ' + err.message
-         );
-         errorThrown = true;
-       }
-       if (!errorThrown) {
-         assert.fail('No error thrown.');
-       }
-     });
-  it('should revert with a insufficient-value error', async () => {
-    let errorThrown = false;
-    const amounts = Array(nonDeployerAccounts.length).fill('1');
-    const instance = await Coinosis.deployed();
-    const value = nonDeployerAccounts.length - 1;
-    try {
-      await instance.distribute(nonDeployerAccounts, amounts, { value });
+
+  contract('distribute()', async accounts => {
+    const nonDeployerAccounts = accounts.slice(1);
+    const getBalances = async accounts => {
+      const balances = [];
+      for (let i = 0; i < accounts.length; i++) {
+        const account = accounts[i];
+        const balance = await web3.eth.getBalance(account);
+        balances.push(BigInt(balance));
+      }
+      return balances;
     }
-    catch (err) {
-      assert.include(
-        err.message,
-        'insufficient-value',
-        'The error thrown isn\'t insufficient-value: ' + err.message
-      );
-      errorThrown = true;
-    }
-    if (!errorThrown) {
-      assert.fail('No error thrown.');
-    }
+    it('should distribute one wei to every non-deployer account', async () => {
+      const initialBalances = await getBalances(nonDeployerAccounts);
+      const amounts = Array(nonDeployerAccounts.length).fill('1');
+      const instance = await Coinosis.deployed();
+      await instance.distribute(nonDeployerAccounts, amounts, {
+        value: String(nonDeployerAccounts.length),
+      });
+      const finalBalances = await getBalances(nonDeployerAccounts);
+      for (let i = 0; i < nonDeployerAccounts.length; i++) {
+        const initialBalance = initialBalances[i];
+        const finalBalance = finalBalances[i];
+        assert.equal(
+          finalBalance,
+          initialBalance + 1n
+        );
+      }
+    });
+    it('should revert with a different-lengths error',
+       async () => {
+         let errorThrown = false;
+         const instance = await Coinosis.deployed();
+         const amounts = Array(nonDeployerAccounts.length - 1).fill('1');
+         const value = nonDeployerAccounts.length * 2;
+         try {
+           await instance.distribute(nonDeployerAccounts, amounts, { value });
+         }
+         catch (err) {
+           assert.include(
+             err.message,
+             'different-lengths',
+             'The error thrown isn\'t different-lengths: ' + err.message
+           );
+           errorThrown = true;
+         }
+         if (!errorThrown) {
+           assert.fail('No error thrown.');
+         }
+       });
+    it('should revert with a insufficient-value error', async () => {
+      let errorThrown = false;
+      const amounts = Array(nonDeployerAccounts.length).fill('1');
+      const instance = await Coinosis.deployed();
+      const value = nonDeployerAccounts.length - 1;
+      try {
+        await instance.distribute(nonDeployerAccounts, amounts, { value });
+      }
+      catch (err) {
+        assert.include(
+          err.message,
+          'insufficient-value',
+          'The error thrown isn\'t insufficient-value: ' + err.message
+        );
+        errorThrown = true;
+      }
+      if (!errorThrown) {
+        assert.fail('No error thrown.');
+      }
+    });
   });
 });
