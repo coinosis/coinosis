e7f55464e2ca7019804fd05306b6d1bd20651795
Emilio Silva Schlenker <emilio@autark.xyz>
2020-04-19 20:41:34 -0500

    using SafeMath

--- contracts/Coinosis.sol
+++ contracts/Coinosis.sol
@@ -1,7 +1,11 @@
 pragma solidity 0.5.16;
 pragma experimental ABIEncoderV2;
 
+import "@openzeppelin/contracts/math/SafeMath.sol";
+
 contract Coinosis {
+
+    using SafeMath for uint;
 
     string constant NOT_OWNER = "The sender account is not the owner \
 (not-owner)";
@@ -16,6 +20,7 @@ contract Coinosis {
     string constant INSUFFICIENT_VALUE =
         "The ether value in this contract is less than the total reward value \
 to send (insufficient-value)";
+
     address private owner;
 
     event Assessment(
@@ -50,22 +55,22 @@ to send (insufficient-value)";
         string[] memory names,
         address payable[] memory addresses,
         uint[] memory claps
-    ) public { // TODO: use SafeMath
+    ) public {
         require(msg.sender == owner, NOT_OWNER);
         require(ETHPriceUSDWei > 0, INVALID_ETH_PRICE);
         require(names.length == addresses.length, NAMES_DIFFER_ADDRESSES);
         require(addresses.length == claps.length, ADDRESSES_DIFFER_CLAPS);
         uint registrationPriceWei =
-            registrationPriceUSDWei * 1 ether / ETHPriceUSDWei;
-        uint totalPriceWei = registrationPriceWei * addresses.length;
+            registrationPriceUSDWei.mul(1 ether).div(ETHPriceUSDWei);
+        uint totalPriceWei = registrationPriceWei.mul(addresses.length);
         require(address(this).balance >= totalPriceWei, INSUFFICIENT_VALUE);
         uint totalClaps = 0;
-        for (uint i = 0; i < claps.length; i++) {
-            totalClaps += claps[i];
+        for (uint i = 0; i < claps.length; i = i.add(1)) {
+            totalClaps = totalClaps.add(claps[i]);
         }
         uint[] memory rewards = new uint[](claps.length);
-        for (uint i = 0; i < claps.length; i++) {
-            rewards[i] = claps[i] * totalPriceWei / totalClaps;
+        for (uint i = 0; i < claps.length; i = i.add(1)) {
+            rewards[i] = claps[i].mul(totalPriceWei).div(totalClaps);
         }
         emit Assessment(
             registrationPriceUSDWei,
@@ -78,7 +83,7 @@ to send (insufficient-value)";
             totalClaps,
             rewards
         );
-        for (uint i = 0; i < addresses.length; i++) {
+        for (uint i = 0; i < addresses.length; i = i.add(1)) {
             if (rewards[i] > 0 && addresses[i].send(rewards[i])) {
                 emit Transfer(
                     names[i],
