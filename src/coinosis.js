55dba2088e2eab752a3d0e0ff14fadb98bf3d08d
Emilio Silva Schlenker <emilio@autark.xyz>
2020-04-21 21:48:16 -0500

    front-end fully functional, no styling

--- src/coinosis.js
+++ src/coinosis.js
@@ -1,70 +1,336 @@
-import React, { useEffect, useState } from 'react';
+import React, { createContext, useContext, useEffect, useState } from 'react';
 import Web3 from 'web3';
-import coinosisJson from '../build/contracts/Coinosis.json';
+import contractJson from '../build/contracts/Coinosis.json';
 
-const coinosisAddress = '0xBf660E18795cC6167cB4a5C71c7708EF6D91dED8';
+const contractAddress = '0xEE84c6766F1C60D4abF2171CC433B7c13e3084d3';
+const Web3Context = createContext();
+const ContractContext = createContext();
+
+const usdPlaceholder = ' '.repeat(4);
+const ethPlaceholder = ' '.repeat(5);
+const datePlaceholder = ' '.repeat(21);
+const percentagePlaceholder = ' '.repeat(4);
 
 const Coinosis = () => {
-  const [name, setName] = useState('');
-  const [amount, setAmount] = useState('');
-  const [account, setAccount] = useState();
+
+  const [web3, setWeb3] = useState();
   const [contract, setContract] = useState();
-  const [events] = useState([]);
+
   useEffect(() => {
     const web3 = new Web3(Web3.givenProvider);
-    web3.eth.requestAccounts().then(accounts => setAccount(accounts[0]));
-    const contract = new web3.eth.Contract(coinosisJson.abi, coinosisAddress);
+    setWeb3(web3);
+    const contract = new web3.eth.Contract(contractJson.abi, contractAddress);
     setContract(contract);
-    contract.events.Received({fromBlock: 0}, (error, event) => {
+  }, []);
+
+  if(!web3) return <Loading/>
+
+  return (
+    <Web3Context.Provider value={web3}>
+      <ContractContext.Provider value={contract}>
+        <ContractInfo />
+        <Assessments />
+      </ContractContext.Provider>
+    </Web3Context.Provider>
+  );
+}
+
+const ContractInfo = ({}) => {
+  return (
+    <div>
+      <div>
+        contrato
+      </div>
+      <div>
+        <Link type="address" data={contractAddress}>
+          {contractAddress}
+        </Link>
+      </div>
+    </div>
+  );
+}
+
+const Assessments = ({}) => {
+  
+  const contract = useContext(ContractContext);
+  const [assessments, setAssessments] = useState([]);
+
+  useEffect(() => {
+    contract.events.Assessment({fromBlock: 0}, (error, event) => {
       if (error) {
         console.error(error);
         return;
       }
-      const { name, amount, sender } = event.returnValues;
-      const newEvent = { name, amount, sender };
-      events.push(newEvent);
+      event.returnValues.id = event.transactionHash;
+      setAssessments(assessments => [ ...assessments, event.returnValues ]);
     });
   }, []);
-  const submit = () => {
-    contract.methods.receive(name).send({value: amount, from: account});
-    setName('');
-    setAmount('');
-  }
-  console.log(events.length);
+
+  return (
+    <div>
+      {assessments.map(assessment => {
+        return (
+          <Assessment key={assessment.id} { ...assessment } />
+        );
+      })}
+    </div>
+  );
+  
+}
+
+const Assessment = ({
+  id,
+  timestamp,
+  registrationFeeUSDWei,
+  ETHPriceUSDWei,
+  names,
+  addresses,
+  claps,
+  registrationFeeWei,
+  totalFeesWei,
+  totalClaps,
+  rewards
+}) => {
+
+  const contract = useContext(ContractContext);
+  const [transfers, setTransfers] = useState([]);
+
+  useEffect(() => {
+    contract.events.Transfer(
+      { fromBlock: 0, filter: {transactionHash: id} },
+      (error, event) => {
+        if (error) {
+          console.error(error);
+          return;
+        }
+        event.returnValues.transactionHash = event.transactionHash;
+        setTransfers(transfers => [ ...transfers, event.returnValues ]);
+      });
+  }, []);
+
+  return (
+    <div>
+      <Header
+        id={id}
+        timestamp={timestamp}
+        addresses={addresses}
+        registrationFeeUSDWei={registrationFeeUSDWei}
+        registrationFeeWei={registrationFeeWei}
+        ETHPriceUSDWei={ETHPriceUSDWei}
+        totalFeesWei={totalFeesWei}
+        totalClaps={totalClaps}
+      />
+      <div>
+        <table>
+          <thead>
+            <tr>
+              <th>participante</th>
+              <th>aplausos</th>
+              <th>porcentaje</th>
+              <th>recompensa</th>
+              <th>balance</th>
+              <th>estado</th>
+            </tr>
+          </thead>
+          <tbody>
+            {addresses.map((address, i) => {
+              const transferFilter = transfers.filter(transfer => (
+                transfer.addr === address
+              ));
+              return (
+                <Participant
+                  key={address}
+                  name={names[i]}
+                  address={addresses[i]}
+                  claps={claps[i]}
+                  totalClaps={totalClaps}
+                  reward={rewards[i]}
+                  rate={ETHPriceUSDWei}
+                  registrationFeeWei={registrationFeeWei}
+                  ETHPriceUSDWei={ETHPriceUSDWei}
+                  transferFilter={transferFilter}
+                />
+              );
+            })}
+
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+}
+
+const Header = ({
+  id,
+  timestamp,
+  addresses,
+  registrationFeeUSDWei,
+  registrationFeeWei,
+  ETHPriceUSDWei,
+  totalFeesWei,
+  totalClaps
+}) => {
   return (
     <div>
       <div>
         <div>
-          <div>
-            Name
-          </div>
-          <input value={name} onChange={e => setName(e.target.value)} />
+          distribución
         </div>
         <div>
-          <div>
-            Amount
-          </div>
-          <input value={amount} onChange={e => setAmount(e.target.value)} />
-        </div>
-        <div>
-          <button onClick={submit}>
-            Submit
-          </button>
+          <Link type="tx" data={id}>
+            {id}
+          </Link>
         </div>
       </div>
       <div>
-        {events.map(event => {
-          return (
-            <div key={event.name}>
-              <div>
-                {event.name}
-              </div>
-            </div>
-          );
-        })}
+        <DateTime timestamp={timestamp} />
+      </div>
+      <div>
+        <div>
+          <div>participantes</div>
+          <div>{addresses.length}</div>
+        </div>
+      </div>
+      <div>
+        <div>aporte por persona</div>
+        <Amount
+          usd={registrationFeeUSDWei}
+          eth={registrationFeeWei}
+          rate={ETHPriceUSDWei}
+        />
+      </div>
+      <div>
+        <div>aporte total</div>
+        <Amount
+          eth={totalFeesWei}
+          rate={ETHPriceUSDWei}
+        />
+      </div>
+      <div>
+        <div>total aplausos</div>
+        <div>{totalClaps}</div>
       </div>
     </div>
   )
+}
+
+const Participant = ({
+  name,
+  address,
+  claps,
+  totalClaps,
+  reward,
+  rate,
+  registrationFeeWei,
+  ETHPriceUSDWei,
+  transferFilter,
+}) => {
+
+  const [percentage, setPercentage] = useState(percentagePlaceholder);
+  const [balance, setBalance] = useState('');
+  const [status, setStatus] = useState('');
+  const [tx, setTx] = useState('');
+
+
+  useEffect(() => {
+    const percentage = 100 * +claps / +totalClaps;
+    setPercentage(percentage.toFixed(1) + ' %');
+    const balance = reward - registrationFeeWei;
+    setBalance(String(balance));
+  }, []);
+
+  useEffect(() => {
+    if (transferFilter.length) {
+      setStatus('enviada');
+      setTx(transferFilter[0].transactionHash);
+    }
+  }, [ transferFilter ]);
+
+  return (
+    <tr>
+      <td>
+        <Link type="address" data={address}>
+          {name}
+        </Link>
+      </td>
+      <td>{claps}</td>
+      <td>{percentage}</td>
+      <td>
+        <Amount eth={reward} rate={rate} />
+      </td>
+      <td>
+        <Amount
+          eth={balance}
+          rate={ETHPriceUSDWei}
+        />
+      </td>
+      <td>
+        <Link type="tx" data={tx}>
+          {status}
+        </Link>
+      </td>
+    </tr>
+  );
+}
+
+const Amount = ({ usd: usdWei, eth: wei, rate: rateWei }) => {
+
+  const web3 = useContext(Web3Context);
+  const [usd, setUSD] = useState(usdPlaceholder);
+  const [eth, setETH] = useState(ethPlaceholder);
+  const [rate, setRate] = useState(usdPlaceholder);
+  
+  useEffect(() => {
+    if(!usdWei) {
+      usdWei = String(Math.round(web3.utils.fromWei(
+        String(BigInt(wei * rateWei))
+      )));
+    }
+    setUSD(Number(web3.utils.fromWei(usdWei)).toFixed(2) + ' USD');
+    setETH(Number(web3.utils.fromWei(wei)).toFixed(3) + ' ETH');
+    setRate(Number(web3.utils.fromWei(rateWei)).toFixed(2) + ' USD/ETH');
+  }, [usdWei, wei, rateWei]);
+
+  return (
+    <div>
+      {usd} ({eth} @ {rate})
+    </div>
+  );
+}
+
+const Link = ({ type, data, children }) => {
+  
+  const [href, setHref] = useState('');
+
+  useEffect(() => {
+    let href = `https://etherscan.io/${type}/${data}`;
+    setHref(href);
+  }, [data]);
+  
+  return (
+    <a href={href} target="_blank">
+      {children}
+    </a>
+  );
+}
+
+const DateTime = ({ timestamp }) => {
+  
+  const [date, setDate] = useState(datePlaceholder);
+
+  useEffect(() => {
+    setDate(new Date(timestamp * 1000).toLocaleString());
+  }, [ timestamp ]);
+
+  return (
+    <div>
+      {date}
+    </div>
+  );
+}
+
+const Loading = ({}) => {
+  return <div>loading...</div>
 }
 
 export default Coinosis
