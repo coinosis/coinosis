95defc78c1e9f8044177abaa66a83b1c8e36f47f
Emilio Silva Schlenker <emilio@autark.xyz>
2020-04-27 19:03:03 -0500

    the docs folder with the built front-end

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,9 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'development',
+  output: {
+    path: path.resolve(__dirname, "docs")
+  },
   devtool: 'inline-source-map',
   module: {
     rules: [
