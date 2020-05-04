9275d79e3d3444a2614a2dfb8d04423e56a73bc1
Emilio Silva Schlenker <emilio@autark.xyz>
2020-05-04 12:11:27 -0500

    moved the Header component to its own file

--- src/coinosis.js
+++ src/coinosis.js
@@ -1,12 +1,12 @@
-import React, { createContext, useContext, useEffect, useState } from 'react';
+import React, { createContext, useEffect, useState } from 'react';
 import Web3 from 'web3';
-import styled, { createGlobalStyle } from 'styled-components';
-import logo from './assets/logo.png';
+import { createGlobalStyle } from 'styled-components';
 import InstallMetamask from './installMetamask';
 import contractJson from '../build/contracts/Coinosis.json';
-import { environment, Hash, Loading, NoContract } from './helpers';
+import { environment, Loading, NoContract } from './helpers';
 import settings from './settings.json';
-import Account from './account';
+
+import Header from './header';
 import Event from './event';
 
 export const Web3Context = createContext();
@@ -82,87 +82,5 @@ const GlobalStyle = createGlobalStyle`
     margin: 0;
   }
 `
-
-const Header = () => {
-
-  return (
-    <div
-      css={`
-        display: flex;
-        justify-content: space-between;
-        align-items: center;
-        background: #fafafa;
-        box-shadow: 0 1px 10px rgba(151, 164, 175, .1);
-        padding: 10px 40px;
-        margin-bottom: 40px;
-      `}
-    >
-      <HeaderItem
-        css={`
-          justify-content: flex-start;
-        `}
-      >
-        <ContractInfo/>
-      </HeaderItem>
-      <HeaderItem
-        css={`
-          justify-content: center;
-          font-size: 26px;
-        `}
-      >
-        <img src={logo} height={45} alt="coinosis" />
-      </HeaderItem>
-      <HeaderItem
-        css={`
-          justify-content: flex-end;
-        `}
-      >
-        <Account/>
-      </HeaderItem>
-    </div>
-  );
-}
-
-const HeaderItem = styled.div`
-  width: 33.333%;
-  display: flex;
-`
-
-const ContractInfo = () => {
-
-  const contract = useContext(ContractContext);
-  const [address, setAddress] = useState('');
-  const [version, setVersion] = useState('');
-
-  useEffect(() => {
-    if (contract) {
-      setAddress(contract._address);
-      contract.methods.version().call().then(setVersion);
-    }
-  }, [contract]);
-
-  return (
-    <div
-      css={`
-        display: flex;
-        justify-content: center;
-      `}
-    >
-      <div>
-        contrato
-      </div>
-      <div
-        css={`
-          margin: 0 5px;
-        `}
-      >
-        <Hash type="address" value={address} toolTipPosition="bottomLeft" />
-      </div>
-      <div>
-        {version}
-      </div>
-    </div>
-  );
-}
 
 export default Coinosis
---
+++ src/header.js
@@ -0,0 +1,90 @@
+import React, { useContext, useEffect, useState } from 'react';
+import styled from 'styled-components';
+import { Hash } from './helpers';
+import logo from './assets/logo.png';
+import { ContractContext } from './coinosis';
+import Account from './account';
+
+const Header = () => {
+
+  return (
+    <div
+      css={`
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        background: #fafafa;
+        box-shadow: 0 1px 10px rgba(151, 164, 175, .1);
+        padding: 10px 40px;
+        margin-bottom: 40px;
+      `}
+    >
+      <HeaderItem
+        css={`
+          justify-content: flex-start;
+        `}
+      >
+        <ContractInfo/>
+      </HeaderItem>
+      <HeaderItem
+        css={`
+          justify-content: center;
+          font-size: 26px;
+        `}
+      >
+        <img src={logo} height={45} alt="coinosis" />
+      </HeaderItem>
+      <HeaderItem
+        css={`
+          justify-content: flex-end;
+        `}
+      >
+        <Account/>
+      </HeaderItem>
+    </div>
+  );
+}
+
+const HeaderItem = styled.div`
+  width: 33.333%;
+  display: flex;
+`
+
+const ContractInfo = () => {
+
+  const contract = useContext(ContractContext);
+  const [address, setAddress] = useState('');
+  const [version, setVersion] = useState('');
+
+  useEffect(() => {
+    if (contract) {
+      setAddress(contract._address);
+      contract.methods.version().call().then(setVersion);
+    }
+  }, [contract]);
+
+  return (
+    <div
+      css={`
+        display: flex;
+        justify-content: center;
+      `}
+    >
+      <div>
+        contrato
+      </div>
+      <div
+        css={`
+          margin: 0 5px;
+        `}
+      >
+        <Hash type="address" value={address} toolTipPosition="bottomLeft" />
+      </div>
+      <div>
+        {version}
+      </div>
+    </div>
+  );
+}
+
+export default Header;
