d714b541b672d26c0840e47050641803c58b30bf
Emilio Silva Schlenker <emilio@autark.xyz>
2020-05-06 21:49:15 -0500

    restricted event creation to privileged accounts

--- src/eventList.js
+++ src/eventList.js
@@ -1,12 +1,21 @@
 import React, { useContext, useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-import { BackendContext } from './coinosis';
+import { AccountContext, BackendContext } from './coinosis';
 import AddEvent from './addEvent';
+
+const privilegedAccounts = [
+  '0xeFaC568c637201ea0A944b888b8FB98386eF2882',
+  '0xfE1d177037DF1ABbdde4c0E4AFcdE9447F8511D0',
+  '0x51e9047a6bBEC3c2a4C03c27382381B129e99e0E',
+  '0xbED9793fC4FEe638805464A16c11ef642e16974D',
+];
 
 const EventList = () => {
 
+  const { account } = useContext(AccountContext);
   const backendURL = useContext(BackendContext);
   const [events, setEvents] = useState([]);
+  const [show, setShow] = useState(false);
 
   useEffect(() => {
     fetch(`${backendURL}/events`)
@@ -22,10 +31,19 @@ const EventList = () => {
       });
   }, []);
 
+  useEffect(() => {
+    if (privilegedAccounts.includes(account)) {
+      setShow(true);
+    }
+    else {
+      setShow(false);
+    }
+  }, [account]);
+
   return (
     <div>
       <div>
-        <AddEvent show setEvents={setEvents} />
+        <AddEvent show={show} setEvents={setEvents} />
       </div>
       <div>
         {events.map(event => {
