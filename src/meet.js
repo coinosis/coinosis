0b22675c8d582088fc4f2dbab5c8f7f6aadd5375
Emilio Silva Schlenker <emilio@autark.xyz>
2020-05-13 02:02:01 -0500

    added link inside the SPA to the webrtc explanation

--- src/meet.js
+++ src/meet.js
@@ -115,42 +115,57 @@ const Meet = ({
   );
 
   return (
-    <Jitsi
-      roomName={id}
-      displayName={userName}
-      userInfo={{ displayName: userName }}
-      noSSL={false}
-      loadingComponent={Loading}
-      onAPILoad={handleAPI}
-      containerStyle={{ width: '100%', height: '800px' }}
-      config={{
-        startWithAudioMuted: true,
-        fileRecordingsEnabled: false,
-        remoteVideoMenu: {
-          disableKick: true,
-        },
-      }}
-      interfaceConfig={{
-        DEFAULT_BACKGROUND: '#476047',
-        TOOLBAR_BUTTONS: [
-          'microphone',
-          'camera',
-          'desktop',
-          'chat',
-          'livestreaming',
-          'raisehand',
-          'videoquality',
-          'stats',
-          'shortcuts',
-          'tileview',
-          'mute-everyone',
-          'settings',
-        ],
-        SETTINGS_SECTIONS: ['language'],
-        SHOW_CHROME_EXTENSION_BANNER: false,
-        ENFORCE_NOTIFICATION_AUTO_DISMISS_TIMEOUT: 15000,
-      }}
-    />
+    <div
+      css={`
+        width: 100%;
+        height: 800px;
+      `}
+    >
+      <div
+        css={`
+          text-align: right;
+        `}
+      >
+      ¿Tienes problemas con el audio y el video? Haz clic
+        <a css="margin: 5px" href="webrtc.html" target="_blank">aquí</a>.
+      </div>
+      <Jitsi
+        roomName={id}
+        displayName={userName}
+        userInfo={{ displayName: userName }}
+        noSSL={false}
+        loadingComponent={Loading}
+        onAPILoad={handleAPI}
+        containerStyle={{ width: '100%', height: '800px' }}
+        config={{
+          startWithAudioMuted: true,
+          fileRecordingsEnabled: false,
+          remoteVideoMenu: {
+            disableKick: true,
+          },
+        }}
+        interfaceConfig={{
+          DEFAULT_BACKGROUND: '#476047',
+          TOOLBAR_BUTTONS: [
+            'microphone',
+            'camera',
+            'desktop',
+            'chat',
+            'livestreaming',
+            'raisehand',
+            'videoquality',
+            'stats',
+            'shortcuts',
+            'tileview',
+            'mute-everyone',
+            'settings',
+          ],
+          SETTINGS_SECTIONS: ['language'],
+          SHOW_CHROME_EXTENSION_BANNER: false,
+          ENFORCE_NOTIFICATION_AUTO_DISMISS_TIMEOUT: 15000,
+        }}
+      />
+    </div>
   );
 }
 
