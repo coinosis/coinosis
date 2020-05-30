ee73eb75a3d3acacd6a63bca43afabacd925bae6
Emilio Silva Schlenker <emilio@autark.xyz>
2020-05-30 08:36:54 -0500

    register and clap functions implemented & tested

--- contracts/Event.sol
+++ contracts/Event.sol
@@ -7,37 +7,54 @@ contract Event {
     using SafeMath for uint256;
 
     string constant private version = "2.0.0";
+    uint8 constant private ATTENDEE_UNREGISTERED = 0;
+    uint8 constant private ATTENDEE_REGISTERED = 1;
+    uint8 constant private ATTENDEE_CLAPPED = 2;
+    uint256 constant public CLAPS_PER_ATTENDEE = 3;
     string constant private WRONG_FEE = "wrong-fee";
     string constant private ALREADY_REGISTERED = "already-registered";
     string constant private UNAUTHORIZED = "unauthorized";
-    string constant private WRONG_CLAPS = "wrong-claps";
+    string constant private DIFFERENT_LENGTHS = "different-lengths";
+    string constant private TOO_MANY_ATTENDEES = "too-many-attendees";
+    string constant private TOO_MANY_CLAPS = "too-many-claps";
 
     string public id;
     uint64 public fee;
     address payable[] public attendees;
     mapping(address => uint8) public states;
     mapping(address => uint256) public claps;
+    uint256 public allowedClaps;
 
     constructor (string memory _id, uint64 _fee) public {
         id = _id;
         fee = _fee;
+        allowedClaps = 0;
     }
 
     function register() public payable {
         require(msg.value == fee, WRONG_FEE);
-        require(states[msg.sender] == 0, ALREADY_REGISTERED);
-        states[msg.sender] = 1;
+        require(
+            states[msg.sender] == ATTENDEE_UNREGISTERED,
+            ALREADY_REGISTERED
+        );
+        require(attendees.length < 100, TOO_MANY_ATTENDEES);
+        states[msg.sender] = ATTENDEE_REGISTERED;
         attendees.push(msg.sender);
+        allowedClaps = allowedClaps.add(CLAPS_PER_ATTENDEE);
     }
 
     function clap(address[] memory _attendees, uint256[] memory _claps)
         public {
-        require(states[msg.sender] == 1, UNAUTHORIZED);
-        require(_attendees.length == _claps.length, WRONG_CLAPS);
-        states[msg.sender] = 2;
+        require(states[msg.sender] == ATTENDEE_REGISTERED, UNAUTHORIZED);
+        require(_attendees.length == _claps.length, DIFFERENT_LENGTHS);
+        states[msg.sender] = ATTENDEE_CLAPPED;
+        uint256 totalClaps = 0;
         for (uint256 i = 0; i < _attendees.length; i = i.add(1)) {
+            totalClaps = totalClaps.add(_claps[i]);
             if (_attendees[i] == msg.sender) continue;
+            if (states[_attendees[i]] == ATTENDEE_UNREGISTERED) continue;
             claps[_attendees[i]] = claps[_attendees[i]].add(_claps[i]);
         }
+        require(totalClaps <= allowedClaps, TOO_MANY_CLAPS);
     }
 }
--- test/event.js
+++ test/event.js
@@ -11,8 +11,9 @@ contract('Event', async accounts => {
     it('succeeds', async () => {
       const instance = await Event.new(id, fee);
       assert.ok(instance.address);
+      assert.equal(id, await instance.id());
+      assert.equal(fee, await instance.fee());
     });
-
 
     it('fails due to wrong number of arguments', async () => {
       await truffleAssert.fails(
@@ -37,8 +38,11 @@ contract('Event', async accounts => {
     it('succeeds', async () => {
       const instance = await Event.new(id, fee);
       await instance.register({from: accounts[0], value: fee});
+      await instance.register({from: accounts[1], value: fee});
+      await instance.register({from: accounts[2], value: fee});
       assert.equal(1, await instance.states(accounts[0]));
       assert.equal(accounts[0], await instance.attendees(0));
+      assert.equal(9, await instance.allowedClaps());
     });
 
     it('fails due to wrong fee', async () => {
@@ -67,13 +71,110 @@ contract('Event', async accounts => {
       const instance = await Event.new(id, fee);
       await instance.register({from: accounts[0], value: fee});
       await instance.register({from: accounts[1], value: fee});
-      await instance.clap([accounts[1]], [5], {from: accounts[0]});
-      const claps = await instance.claps(accounts[1]);
-      assert.equal(5, claps.toNumber());
+      await instance.register({from: accounts[2], value: fee});
+      await instance.clap(
+        [accounts[1], accounts[2]],
+        [3, 6],
+        {from: accounts[0]}
+      );
+      await instance.clap(
+        [accounts[0], accounts[2]],
+        [9, 0],
+        {from: accounts[1]}
+      );
+      await instance.clap(
+        [accounts[0], accounts[1]],
+        [1, 0],
+        {from: accounts[2]}
+      );
+      const claps0 = await instance.claps(accounts[0]);
+      assert.equal(10, claps0.toNumber());
+      const claps1 = await instance.claps(accounts[1]);
+      assert.equal(3, claps1.toNumber());
+      const claps2 = await instance.claps(accounts[2]);
+      assert.equal(6, claps2.toNumber());
     });
 
     it('fails due to not having registered', async () => {
+      const instance = await Event.new(id, fee);
+      await truffleAssert.reverts(
+        instance.clap([accounts[1]], [3], {from: accounts[0]}),
+        'unauthorized'
+      );
+    });
 
+    it('fails due to having already clapped', async () => {
+      const instance = await Event.new(id, fee);
+      await instance.register({from: accounts[0], value: fee});
+      await instance.register({from: accounts[1], value: fee});
+      await instance.clap([accounts[1]], [3], {from: accounts[0]});
+      await truffleAssert.reverts(
+        instance.clap([accounts[1]], [3], {from: accounts[0]}),
+        'unauthorized'
+      );
+    });
+
+    it('fails due to different lengths', async () => {
+      const instance = await Event.new(id, fee);
+      await instance.register({from: accounts[0], value: fee});
+      await instance.register({from: accounts[1], value: fee});
+      await truffleAssert.reverts(
+        instance.clap([accounts[1]], [3, 2], {from: accounts[0]}),
+        'different-lengths'
+      );
+    });
+
+    it('silently disallows clapping for oneself', async () => {
+      const instance = await Event.new(id, fee);
+      await instance.register({from: accounts[0], value: fee});
+      await instance.clap([accounts[0]], [3], {from: accounts[0]});
+      const claps = await instance.claps(accounts[0]);
+      assert.equal(0, claps.toNumber());
+    });
+
+    it('silently disallows clapping for unregistered attendees', async () => {
+      const instance = await Event.new(id, fee);
+      await instance.register({from: accounts[0], value: fee});
+      await instance.clap([accounts[1]], [3], {from: accounts[0]});
+      const claps = await instance.claps(accounts[1]);
+      assert.equal(0, claps.toNumber());
+    });
+
+    it('fails due to too many claps', async () => {
+      const instance = await Event.new(id, fee);
+      await instance.register({from: accounts[0], value: fee});
+      await instance.register({from: accounts[1], value: fee});
+      await instance.register({from: accounts[2], value: fee});
+      await truffleAssert.reverts(
+        instance.clap([accounts[1], accounts[2]], [7, 3], {from: accounts[0]}),
+        'too-many-claps'
+      );
+    });
+
+    it('fails due to negative claps underflow', async () => {
+      const instance = await Event.new(id, fee);
+      await instance.register({from: accounts[0], value: fee});
+      await instance.register({from: accounts[1], value: fee});
+      await truffleAssert.reverts(
+        instance.clap([accounts[1]], [-3], {from: accounts[0]}),
+        'too-many-claps'
+      );
+    });
+
+    // use `ganache-cli -a 100` to stress test it
+    it('many attendees clapping at the same time', async () => {
+      const fee = 1;
+      const instance = await Event.new(id, fee);
+      for (let i = 0; i < accounts.length; i++) {
+        await instance.register({from: accounts[i], value: fee});
+      }
+      for (let i = 0; i < accounts.length; i++) {
+        await instance.clap(
+          accounts,
+          accounts.map(a => Math.floor(Math.random() * 4)),
+          {from: accounts[i]}
+        );
+      }
     });
 
   });
