f34f1aeb0dc575ec9a0f5c65ae3547ac47fe09ce
Emilio Silva Schlenker <e18r@disroot.org>
2020-06-18 12:07:14 -0500

    support for older contract deployment

--- migrations/2_coinosis.js
+++ migrations/2_coinosis.js
@@ -1,5 +1,7 @@
 const Coinosis = artifacts.require("Coinosis");
+const CoinosisV0 = artifacts.require("CoinosisV0");
 
 module.exports = function(deployer) {
   deployer.deploy(Coinosis);
+  deployer.deploy(CoinosisV0);
 };
