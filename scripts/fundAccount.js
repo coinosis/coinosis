3508aa5861af115ec536145505409b6ec5d455fd
Emilio Silva Schlenker <e18r@disroot.org>
2020-06-19 08:40:40 -0500

    fund owl account when starting

--- scripts/fundAccount.js
+++ scripts/fundAccount.js
@@ -1,11 +1,11 @@
 module.exports = async callback => {
   const recipient = (process.argv.length > 4 && process.argv[4])
-        || '0xe1fF19182deb2058016Ae0627c1E4660A895196a';
+        || '0x9fCA45d4fF08e62B3084a68353BE55bD905c961A';
   const accounts = await web3.eth.getAccounts();
   const txObject = {
     from: accounts[0],
     to: recipient,
-    value: web3.utils.toWei('1'),
+    value: web3.utils.toWei('10'),
     gasPrice: web3.utils.toWei('100', 'gwei'),
   }
   const result = await web3.eth.sendTransaction(txObject);
--- start.dev.sh
+++ start.dev.sh
@@ -1,3 +1,6 @@
 #! /bin/bash
 
-ganache-cli --networkId 1337 --mnemonic "$(cat .secret)" --blockTime 30
+ganache-cli --networkId 1337 --mnemonic "$(cat .secret)" --blockTime 30 &
+GANACHE_PID=$!
+truffle exec scripts/fundAccount.js
+wait $GANACHE_PID
