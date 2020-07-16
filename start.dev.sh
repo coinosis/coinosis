ea544adb19ea7f9b767737e15683a6ac4a9d396d
Emilio Silva Schlenker <e18r@disroot.org>
2020-07-15 21:20:04 -0500

    make block time be 15 seconds

--- start.dev.sh
+++ start.dev.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-ganache-cli --networkId 1337 --blockTime 30 $(cat .accounts) &
+ganache-cli --networkId 1337 --blockTime 15 $(cat .accounts) &
 GANACHE_PID=$!
 truffle exec scripts/fundAccount.js
 wait $GANACHE_PID
