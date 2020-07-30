530c3a9b8378655cf564ec3db14bcffdd938d928
Emilio Silva Schlenker <e18r@disroot.org>
2020-07-30 11:09:17 -0500

    use random network ids to prevent nonce errors

--- start.dev.sh
+++ start.dev.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-ganache-cli --networkId 1337 --blockTime 15 $(cat .accounts) &
+ganache-cli --blockTime 15 $(cat .accounts) &
 GANACHE_PID=$!
 truffle exec scripts/fundAccount.js
 wait $GANACHE_PID
